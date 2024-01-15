1e47a9f0319c2859a530ea4655779634c2039dda
necelentano <necelentano@gmail.com>
2024-01-15 14:48:37 +0200

    add: getCurrentUser server action

---
+++ app/actions/getCurrentUser.ts
@@ -0,0 +1,34 @@
+// here it is not an api call, it's direct communication with DB through our server component
+import { getServerSession } from "next-auth/next";
+
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
+import prisma from "@/app/libs/prismadb";
+
+export async function getSession() {
+  return await getServerSession(authOptions);
+}
+
+export default async function getCurrentUser() {
+  // actually, we can write this logic directly in our server component app/layout.tsx Rootlayout
+  try {
+    const session = await getSession();
+
+    if (!session?.user?.email) {
+      return null;
+    }
+
+    const currentUser = await prisma.user.findUnique({
+      where: {
+        email: session.user.email as string,
+      },
+    });
+
+    if (!currentUser) {
+      return null;
+    }
+
+    return currentUser;
+  } catch (error: any) {
+    return null;
+  }
+}
