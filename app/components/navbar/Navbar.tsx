56ee0c47f4fbc7bc5259306e444cb2cc28e52a80
necelentano <necelentano@gmail.com>
2024-01-15 14:50:18 +0200

    add: use getCurrentUser in the app (Navbar, UserMenu)

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -5,8 +5,13 @@ import Container from "../container/Container";
 import Logo from "./logo/Logo";
 import Search from "./search/Search";
 import UserMenu from "./user-menu/UserMenu";
+import { User } from "@prisma/client";
 
-const Navbar = () => {
+interface NavbarProps {
+  currentUser?: User | null;
+}
+
+const Navbar: React.FC<NavbarProps> = ({ currentUser }) => {
   return (
     <div className="fixed w-full bg-white z-10 shadow-sm">
       <div className="py-4 border-b-[1px]">
@@ -14,7 +19,7 @@ const Navbar = () => {
           <div className=" flex flex-row items-center justify-between gap-3 md:gap-0">
             <Logo />
             <Search />
-            <UserMenu />
+            <UserMenu currentUser={currentUser} />
           </div>
         </Container>
       </div>
