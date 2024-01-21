db058e2c27f5a94c20aac429e6199c4ed0746dff
necelentano <necelentano@gmail.com>
2024-01-21 16:45:12 +0200

    add: ListingCard component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,54 @@
-export default function Home() {
-  return <div className="text-rose-500 text-2xl">Hello, AirBnb clone</div>;
+import getCurrentUser from "./actions/getCurrentUser";
+import getListings from "./actions/getListings";
+import Container from "./components/container/Container";
+import EmptyState from "./components/empty-state/EmptyState";
+import ListingCard from "./components/listings/listing-card/ListingCard";
+
+export default async function Home() {
+  const listings = await getListings();
+  const currentUser = await getCurrentUser();
+
+  // basic error-handling
+  if (listings instanceof Error) {
+    return (
+      <Container>
+        <div className="h-[60vh] flex flex-col gap-2 justify-center items-center text-red-600">
+          ERROR: {listings.message}
+        </div>
+      </Container>
+    );
+  }
+
+  if (listings.length === 0) {
+    return (
+      <Container>
+        <EmptyState showReset />
+      </Container>
+    );
+  }
+  return (
+    <Container>
+      <div
+        className="
+          pt-24
+          grid
+          grid-cols-1
+          sm:grid-cols-2
+          md:grid-cols-3
+          lg:grid-cols-4
+          xl:grid-cols-5
+          2xl:grid-cols-6
+          gap-8
+        "
+      >
+        {listings.map((listing) => (
+          <ListingCard
+            key={listing.id}
+            data={listing}
+            currentUser={currentUser}
+          />
+        ))}
+      </div>
+    </Container>
+  );
 }
