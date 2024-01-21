ce0dfe3e80e09a9ace82f945ba7ee8a2569ef6e5
necelentano <necelentano@gmail.com>
2024-01-21 16:44:48 +0200

    add: HearthButton component

---
+++ app/components/hearth-button/HearthButton.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { SafeUser } from "@/app/types";
+import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
+
+interface HearthButtonProps {
+  listingId: string;
+  currentUser?: SafeUser | null;
+}
+
+const HearthButton: React.FC<HearthButtonProps> = ({
+  listingId,
+  currentUser,
+}) => {
+  const hashFavorited = false;
+  const toggleFavorite = () => {};
+
+  return (
+    <div
+      onClick={toggleFavorite}
+      className="relative hover:opacity-80 transition cursor-pointer"
+    >
+      <AiOutlineHeart
+        size={28}
+        className="fill-white absolute -top-[2px] -right-[2px]"
+      />
+      <AiFillHeart
+        size={24}
+        className={`${hashFavorited ? "fill-rose-500" : "fill-neutral-500/70"}`}
+      />
+    </div>
+  );
+};
+
+export default HearthButton;
