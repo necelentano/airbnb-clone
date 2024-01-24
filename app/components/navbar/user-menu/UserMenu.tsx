3c2a46033b7045bad783a6e925d0343e7a57706e
necelentano <necelentano@gmail.com>
2024-01-24 21:34:19 +0200

    add: TripsClient component

--- app/components/navbar/user-menu/UserMenu.tsx
+++ app/components/navbar/user-menu/UserMenu.tsx
@@ -2,6 +2,7 @@
 
 import { useCallback, useState } from "react";
 import { signOut } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 import { AiOutlineMenu } from "react-icons/ai";
 import Avatar from "../../avatar/Avatar";
@@ -17,6 +18,7 @@ interface UserMenuProps {
 }
 
 const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
+  const router = useRouter();
   const registerModal = useRegisterModal();
   const loginModal = useLoginModal();
   const rentModal = useRentModal();
@@ -59,7 +61,10 @@ const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
           <div className="flex flex-col cursor-pointer">
             {currentUser ? (
               <>
-                <MenuItem onClick={() => {}} label="My trips" />
+                <MenuItem
+                  onClick={() => router.push("/trips")}
+                  label="My trips"
+                />
                 <MenuItem onClick={() => {}} label="My favorites" />
                 <MenuItem onClick={() => {}} label="My reservations" />
                 <MenuItem onClick={() => {}} label="My properties" />
