58112b6c553547115f3beb78e8a7a19c3db9f1b7
necelentano <necelentano@gmail.com>
2024-01-25 22:55:55 +0200

    add: improve UX - add toast message & open login modal

--- app/components/modals/register-modal/RegisterModal.tsx
+++ app/components/modals/register-modal/RegisterModal.tsx
@@ -39,7 +39,9 @@ const RegisterModal = () => {
     axios
       .post("/api/register", data)
       .then(() => {
+        toast.success("Success! Now you can log in into your account.");
         registerModal.onClose();
+        loginModal.onOpen();
       })
       .catch((error) => {
         toast.error(error.message);
